ba55972cc6e8b385ecd8678f771ac155cfe6ae96
Alandis <1seals.alandis@gmail.com>
2024-01-14 21:07:08 -0600

    Initial Commit

---
+++ FirstKlassFitness/FirstKlassFitnessApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstKlassFitnessApp.swift
+//  FirstKlassFitness
+//
+//  Created by Alandis Seals on 1/14/24.
+//
+
+import SwiftUI
+
+@main
+struct FirstKlassFitnessApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
