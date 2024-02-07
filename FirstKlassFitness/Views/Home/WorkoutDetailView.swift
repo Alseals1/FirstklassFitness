c9437aec8801405634e95191a26b19df8491f071
Alandis <1seals.alandis@gmail.com>
2024-02-07 11:04:41 -0600

    Clean code

--- FirstKlassFitness/Views/Home/WorkoutDetailView.swift
+++ FirstKlassFitness/Views/Home/WorkoutDetailView.swift
@@ -3,10 +3,17 @@ import SwiftUI
 struct WorkoutDetailView: View {
     var body: some View {
         ScrollView {
-            Image("image")
-                .resizable()
-                .frame(width: .infinity ,height: 500)
-                .aspectRatio(contentMode: .fill)
+            ZStack(alignment: Alignment(horizontal: .leading, vertical: .bottom)) {
+                Image("image")
+                    .resizable()
+                    .frame(width: .infinity ,height: 400)
+                    .aspectRatio(contentMode: .fit)
+                
+                Text("30 Minute Core Thrust")
+                    .font(.system(size: 33, weight: .bold))
+                    .lineLimit(.max)
+                    .padding([.bottom, .leading], 20)
+            }
             
             VStack(spacing: 10) {
                 HStack {
@@ -22,6 +29,16 @@ struct WorkoutDetailView: View {
                 Text("Est accusantium amet nam ipsam blanditiis ut voluptatibus dolorem. Eos obcaecati natus id perferendis quibusdam et explicabo omnis ab asperiores reprehenderit non libero dolore qui ullam dolorum et praesentium quia. Aut voluptatibus dignissimos et error sapiente hic ratione molestiae. Quo internos illo non quae sint nam nihil velit ex provident necessitatibus hic minima aliquam aut quas consectetur et quibusdam ratione.")
                     .font(.system(size: 18, weight: .medium))
                     .multilineTextAlignment(.leading)
+                
+                Button(action: {
+                    
+                }, label: {
+                    Image(systemName: "square.and.pencil")
+                        .font(.system(size: 30, weight: .medium))
+                        .foregroundStyle(.lighterGray)
+                })
+                .frame(maxWidth: .infinity ,alignment: .trailing)
+                
                 
             }
             .padding(.horizontal, 10)
